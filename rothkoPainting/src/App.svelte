3b455ec21be21cefa6849584c378132bfc91edcc
Pranav Rajan <rajan.pranav320@gmail.com>
2023-11-04 15:46:21 -0700

    Finish Rothko painting svelte project

---
+++ rothkoPainting/src/App.svelte
@@ -0,0 +1,21 @@
+<main>
+  <!-- <img
+    src="https://cdn.freecodecamp.org/curriculum/css-box-model/diagram-1.png"
+    alt="box model explanation 1"
+  />
+  <img
+    src="https://cdn.freecodecamp.org/curriculum/css-box-model/diagram-2.png"
+    alt="box model explanation 2"
+  />
+  <img
+    src="https://cdn.freecodecamp.org/curriculum/css-box-model/diagram-3.png"
+    alt="box model explanation 3"
+  /> -->
+  <div class="frame">
+    <div class="canvas">
+      <div class="one" />
+      <div class="two" />
+      <div class="three" />
+    </div>
+  </div>
+</main>
